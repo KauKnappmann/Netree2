8955a7223e6669cb97b0eea16982f1ac2161229e
Mateus V <49762931+CreatorMA@users.noreply.github.com>
2021-02-22 13:59:00 -0300

    TASK0033 - carrinho 99,9% pronto

--- Site/produto/carrinho.php
+++ Site/produto/carrinho.php
@@ -16,31 +16,46 @@ try{
 
 //$toProd = $adm->view();
 
-$html = file_get_contents("carrinho.html");
+session_start();   
+
+if(!isset($_SESSION['carrinho']))
+$_SESSION['carrinho'] = array(array($tipo,$cod,$quantidade));
+ else
+ $_SESSION['carrinho'][] = array($tipo,$cod,$quantidade);
+
+
+$html = file_get_contents("carrinho.html"); 
 $infos = $adm->view($tipo);
 
-$infos = $infos[$cod];
-$valorTotalItem=$quantidade*$infos['valor'];
 
-$item = '<tr>
+
+$item="";
+foreach($_SESSION['carrinho'] as $itens){
+
+    $item = $item.'<tr>
                       <td class="product-thumbnail">
-                        <img src="../Upload/'.$tipo.'/'.$infos['img'].'" alt="Image" class="img-fluid">
+                        
+                        <img src="../Upload/'.$itens[0].'/'.$infos[$itens[1]]['img'].'" alt="Image" class="img-fluid">
                       </td>
                       <td class="product-name">
-                        <h2 class="h5 text-black">'.$infos['nome'].'</h2>
+                        
+                        <h2 class="h5 text-black">'.$infos[$itens[1]]['nome'].'</h2>
                       </td>
-                      <td>$'.$infos['valor'].'</td>
+                      
+                      <td>$'.$infos[$itens[1]]['valor'].'</td>
                       <td>                    
-                          <p>'.$quantidade.'</p>
+                          
+                          <p>'.$itens[2].'</p>
                       </td>
-                      <td>'.$valorTotalItem.'</td>
+                      <td>$49.00</td>
                       <td><a href="#" class="btn btn-primary height-auto btn-sm">X</a></td>
                     </tr>';
 
-$total = '<h2 class="h5 text-black">R$ '.$valorTotalItem.'</h2>';
+                    
+                }
 
 $html = str_replace('{{item}}',$item,$html);
-$html = str_replace('{{total}}',$total,$html);
+//$html = str_replace('{{total}}',$total,$html);
 echo $html
 
 ?>
